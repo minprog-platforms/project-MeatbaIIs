41825165cd61aa80eff3936bd5c9c78e026b9f4a
MeatbaIIs <leon.schuijtvlot@live.nl>
2022-05-26 15:45:36 +0200

    upload

--- flaskproject/flaskr/testr.py
+++ flaskproject/flaskr/testr.py
@@ -8,7 +8,7 @@ import os
 bp = Blueprint('testr', __name__)
 
 
-@bp.route('/testr', methods = ('GET', 'sumbit'))
+@bp.route('/testr', methods = ('GET', 'sumbit', 'upload'))
 def testr():
     if request.method=='submit':
         project_link = request.form['project']
@@ -22,10 +22,19 @@ def testr():
 
         #os.system('cmd /k "git clone '+ project_link)
         os.system('cmd /k "date"')
-        return render_template('testr.html')#redirect(url_for('testr'))
+        #return render_template('testr.html')#redirect(url_for('testr'))
+    elif request.method == 'upload':
+       f = request.files['file']
+       f.save(secure_filename(f.filename))
     else:
         return render_template('testr.html')
 
+# @bp.route('/uploader', methods = ['GET', 'upload'])
+# def upload_file():
+#     if request.method == 'upload':
+#        f = request.files['file']
+#        f.save(secure_filename(f.filename))
+#        return render_template('testr.html')
 
 
 
@@ -43,5 +52,3 @@ def testr():
 #             flash(error)
 #
 #         os.system('cmd /k "git clone '+ project_link)
-
-
