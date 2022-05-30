579d2c6fd4208e3674cee5a7f8d5dde3b4cba637
MeatbaIIs <leon.schuijtvlot@live.nl>
2022-05-30 14:37:01 +0200

    cleanup

--- flaskproject/testr/__init__.py
+++ flaskproject/testr/__init__.py
@@ -25,19 +25,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    #a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    from . import db
-    db.init_app(app)
-
-    from . import auth
-    app.register_blueprint(auth.bp)
-
-    from . import blog
-    app.register_blueprint(blog.bp)
 
     from . import testr
     app.register_blueprint(testr.bp)
