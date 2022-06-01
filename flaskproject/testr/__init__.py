edbe95311dc9f9f3e695c89c9c5996a6be2766c6
MeatbaIIs <leon.schuijtvlot@live.nl>
2022-06-01 12:15:20 +0200

    comments

--- flaskproject/testr/__init__.py
+++ flaskproject/testr/__init__.py
@@ -1,17 +1,24 @@
+"""
+Author: Leon Schuijtvlot
+Studentno.: 14291584
+This program is a webpage that can communicate with a webhook on github to
+automaticly test a project.
+"""
+
 import os
 
 from flask import Flask, current_app
 
 
 def create_app(test_config=None):
+    "This function creates the app"
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    #app.app_context().push()
-
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    # )
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
